2c76682d143ef2cb62872c9fed80f76bc86ffde1
Bhaskar Pandya <pandyaharsh852003@gmail.com>
2023-05-10 23:04:15 +0530

    java script logic till save the data in console

--- password generator/app.js
+++ password generator/app.js
@@ -3,6 +3,29 @@ const lowerSet = "abcdefghijklmnopqrstuvwxyz"
 const numberSet = "1234567890"
 const symbolSet = "!@#$%^&*()_+/"
 
+//selector
+
+const passBox = document.getElementByIdTagName("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
 const getRandomData =  (dataSet) => {
     return dataSet[Math.floor(Math.random() * dataSet.length)]
 }  
+
+const generatePassword = (password = "") =>{
+    if(upperInput.checked){
+        password += getRandomData(upperSet)
+    }
+    console.log(password)
+}
+
+document.getElementById("btn").addEventListener{
+    "click",
+    function(){
+        generatePassword()
+    }
+}
